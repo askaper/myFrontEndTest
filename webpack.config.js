9965b6d3fca2776a2129faffd8f91b098f0df389
Andrew Skaper <andrew@skaper.info>
2017-11-09 20:48:04 -0600

    Ran AirBnb style lint rules

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 const appTitle = 'Pizza Challenge';
-const filePath = path.join(__dirname, 'src','index.js');
+const filePath = path.join(__dirname, 'src','index.jsx');
 const PORT = '8080';
 
 const config = {
@@ -18,7 +18,7 @@ const config = {
   ],
   module: {
     rules: [
-      { test: /\.js$/, exclude: /node_modules/, use: [ "babel-loader", "eslint-loader" ] },
+      { test: /\.jsx$/, exclude: /node_modules/, use: [ { loader: "babel-loader" }, { loader: "eslint-loader", options: { fix: true }} ]},
       { test: /\.css$/, use: ['style-loader', 'css-loader'] },
       { test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/, loader: 'url-loader', options: { limit: 100000 } }
     ]
