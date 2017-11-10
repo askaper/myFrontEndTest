97817a051c1c09d9a186298e803615a1b0173ec1
Andrew Skaper <andrew@skaper.info>
2017-11-09 19:26:49 -0600

    Added ESLint with AirBnb styles

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ const config = {
   ],
   module: {
     rules: [
-      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+      { test: /\.js$/, exclude: /node_modules/, use: [ "babel-loader", "eslint-loader" ] },
       { test: /\.css$/, use: ['style-loader', 'css-loader'] },
       { test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/, loader: 'url-loader', options: { limit: 100000 } }
     ]
